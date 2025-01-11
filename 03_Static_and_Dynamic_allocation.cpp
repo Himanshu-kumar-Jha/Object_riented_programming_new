1972925bc6a35d3e0a3e40a3d551494949bd955c
Himanshu <himanshu.sde@gmail.com>
2025-01-11 20:00:26 +0530

    static and dynamic allocation

---
+++ 03_Static_and_Dynamic_allocation.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+class Nation{
+    private:
+    int nuclearWarheads;
+    int nuclearSubramines;
+
+    protected:
+    std::string leaderName;
+
+    public:
+    std::string nameOfTheNation;
+    std::string population;
+    double gdpGrowth;
+    //constructor
+    Nation(std::string name , int nuclearSubmarines , int nuclearWarheads , std::string population , double gdpGrowth, std::string leaderName)
+    {
+        this->nameOfTheNation = name;              //this pointer is used to differentiate between the class member and the parameter passed to the constructor.
+        this->nuclearWarheads = nuclearWarheads;  // here we are accessing a private member of the class using the constructor.
+        this->nuclearSubramines = nuclearSubramines;
+        this->population = population;
+        this->gdpGrowth = gdpGrowth;
+        this->leaderName = leaderName;
+    }
+
+    void updateNuclearWarheads(int newWarheads)   //function to update the number of nuclear warheads which is a private member also a setter function.
+    {
+        nuclearWarheads = newWarheads;
+    }
+    std::string showNameofNation(){
+        return nameOfTheNation;
+    }
+};
+int main(){
+    //dynamic allocation of objects
+    Nation *India = new Nation ("India" , 2 , 120 , "1.5B", 7 , "Narendra Modi");
+    cout<<"Name of the nation is " <<(*India).showNameofNation();
+    //or
+    cout<<"Name of the nation is " <<India->showNameofNation();
+    //static allocation of objects
+    Nation USA("USA" , 5 , 200 , "0.5B", 5 , "Donald Trump");
+    cout<<"Name of the nation is " <<USA.showNameofNation();
+    return 0;
+}
