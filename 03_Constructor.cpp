d6ad3ab6510123e9a6d4158497fa43c51ac1c9c7
Himanshu <himanshu.sde@gmail.com>
2025-01-17 00:54:05 +0530

    copy constructor

--- 03_Constructor.cpp
+++ 03_Constructor.cpp
@@ -19,12 +19,23 @@ public:
         this->HouseName = HouseName;
         this->HouseLocation = HouseLocation;
     }
+    //copy constructor
+    House( const House & House){
+        std::cout<<"Copy constructor called"<<std::endl;
+        LandLordName = House.LandLordName;
+        AreaSquareFeet = House.AreaSquareFeet;
+        HouseName = House.HouseName;
+        HouseLocation = House.HouseLocation;
+    }
 
 };
 int main() {
     House House1("Rajesh" , 2000 , "Rajesh Villa" , "Bangalore");
     House House2;
     std::cout<<"The address of the object is "<<&House2<<std::endl;
+    //copy constructor
+    House House3 = House1;
+
 
     return 0;
 }
